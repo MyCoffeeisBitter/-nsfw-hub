49ea59032b750b19687f23507d41256cdc12af9e
MyCoffeeisBitter <141412040+MyCoffeeisBitter@users.noreply.github.com>
2024-07-21 01:40:16 +0800

    Update app.js
    creating a res.redirect to rule 34

--- nsfwHub/app.js
+++ nsfwHub/app.js
@@ -35,6 +35,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get('/', (req, res)=>{
+  res.redirect('/r34');
+})
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
